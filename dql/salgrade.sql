d12ddfba4aff14c039dceaff4e1d3d71fcc10356
ShuwenHe <1201220707@pku.edu.cn>
2020-12-08 14:51:15 +0800

    feat: dept-emp-salgrade

---
+++ dql/salgrade.sql
@@ -0,0 +1,41 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : shuwenshop
+ Source Server Type    : MySQL
+ Source Server Version : 50647
+ Source Host           : 62.234.11.179:3306
+ Source Schema         : shuwenshop
+
+ Target Server Type    : MySQL
+ Target Server Version : 50647
+ File Encoding         : 65001
+
+ Date: 08/12/2020 14:50:04
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for salgrade
+-- ----------------------------
+DROP TABLE IF EXISTS `salgrade`;
+CREATE TABLE `salgrade` (
+  `GRADE` int(11) DEFAULT NULL,
+  `LOSAL` int(11) DEFAULT NULL,
+  `HISAL` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of salgrade
+-- ----------------------------
+BEGIN;
+INSERT INTO `salgrade` VALUES (1, 700, 1200);
+INSERT INTO `salgrade` VALUES (2, 1201, 1400);
+INSERT INTO `salgrade` VALUES (3, 1401, 2000);
+INSERT INTO `salgrade` VALUES (4, 2001, 3000);
+INSERT INTO `salgrade` VALUES (5, 3001, 9999);
+COMMIT;
+
+SET FOREIGN_KEY_CHECKS = 1;
